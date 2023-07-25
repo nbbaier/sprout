c8a046ee81cc5804b047a42818855bd36301fcd3
Nico Baier <nico.baier@gmail.com>
2023-07-25 01:25:45 -0500

    added packages, edited index.js

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import fse from "fs-extra";
+import yaml from "yaml";
+
+import mustache from "mustache";
+
+const name = process.argv[2] || "project";
+
+// const structure = {
+//   dir: "{name}}",
+//   children: [
+//     { dir: "src", children: [{ dir: "name" }] },
+//     { dir: "tests", children: [{ file: "file2.txt" }] },
+//     { file: "file1.txt" },
+//   ],
+// };
+
+const parsed = yaml.parse(fse.readFileSync("./src/file.yaml", "utf8"));
+
+function createDirectory(obj, parentDir = ".") {
+  const dirname =
+    obj.dir && obj.dir.includes("{")
+      ? mustache.render(`${obj.dir}`, { name })
+      : obj.dir;
+
+  const dirPath = parentDir + "/" + dirname;
+  fse.mkdirpSync(dirPath);
+
+  if (obj.children && obj.children.length > 0) {
+    for (const child of obj.children) {
+      if (child.dir) {
+        createDirectory(child, dirPath);
+      }
+      if (child.file) {
+        const filename =
+          child.file && child.file.includes("{")
+            ? mustache.render(`${child.file}`, { name })
+            : child.file;
+
+        const filePath = dirPath + "/" + filename;
+        fse.writeFileSync(filePath, "");
+      }
+    }
+  }
+}
+
+createDirectory(parsed);
